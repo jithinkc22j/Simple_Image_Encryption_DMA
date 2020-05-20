ac907be3990cb141de194b8d2b40cfedcbf7196e
JITHIN K C <jithinkc22@gmail.com>
2020-05-20 13:03:50 +0530

    Add files via upload

---
+++ arnold.m
@@ -0,0 +1,23 @@
+function [outImg]=arnold(o,p,siz)
+s = siz;
+l=siz;
+H = double(zeros(siz,siz,1));
+x=o;
+y=p;
+
+iter=4;
+for i=1:iter
+for c = 1:s
+   
+    for r = 1:l
+   
+     % If you need complete code contact me
+       
+    end
+    
+end 
+end 
+  
+k=H;
+rsiz=siz*siz;
+outImg= reshape(k,[1,rsiz]);
---
+++ md.m
@@ -0,0 +1,31 @@
+function W=md(km,siz)
+col=20;
+m=siz;
+cx=;
+cy=;
+l=;
+
+
+% If you need complete code contact me
+
+Z=zeros(m);
+
+for k=1:col;
+Z=Z.^2+C;
+W=exp(-abs(Z));
+end
+
+
+
+LO=W;
+KE=km;
+for m=1:siz;
+    for j=1:siz;
+        
+        
+        
+        end
+    end
+end
+W=uint8(W);
+end
